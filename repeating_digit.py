2f741d270c383387932b2de655ce98cf7bbc55a4
Krzysztof <krzysztof.majchrzak12@gmail.com>
2018-09-01 18:57:55 +0200

    Pokazuje WSZYSTKIE liczby, które się powtarzają i indeks pierwszej, która się powtarza

--- repeating_digit.py
+++ repeating_digit.py
@@ -1,1 +1,8 @@
-print("let's start") 
+digits = [1, 5, 7, 3, 5, 8, 3, 6]
+for d1 in range(0, len(digits)):
+    for d2 in range(1, len(digits)):
+        if digits[d1] == digits[d2] and d1 != d2:
+            print (digits[d1])
+            
+print ("Indeks pierwszego:",digits.index(d1))
+
