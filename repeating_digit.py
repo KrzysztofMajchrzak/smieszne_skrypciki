7fc2e24321376164b6c5f415c42a685154164dc8
Krzysztof <krzysztof.majchrzak12@gmail.com>
2018-09-04 17:06:27 +0200

    working, yet without indexes

--- repeating_digit.py
+++ repeating_digit.py
@@ -1,8 +1,8 @@
-digits = [1, 5, 7, 3, 5, 8, 3, 6]
-for d1 in range(0, len(digits)):
-    for d2 in range(1, len(digits)):
-        if digits[d1] == digits[d2] and d1 != d2:
-            print (digits[d1])
-            
-print ("Indeks pierwszego:",digits.index(d1))
-
+digits = [1, 7, 1, 6, 3, 1, 1, 7, 3, 6, 8, 8, 9]
+def sprawdzanie(digits):
+    global d1
+    for d1 in range(0, len(digits)):
+        for d2 in range(0, d1):
+            if digits[d1] == digits[d2] and d1 != d2:
+                return digits[d1] 
+print (sprawdzanie(digits))      
