ffb3dd8cfe330c8d1d7dab9e2ed6806a2ed8395f
Krzysztof <krzysztof.majchrzak12@gmail.com>
2018-09-08 23:36:11 +0200

    function and variables names changed

--- repeating_digit.py
+++ repeating_digit.py
@@ -1,8 +1,7 @@
-digits = [1, 7, 1, 6, 3, 1, 1, 7, 3, 6, 8, 8, 9]
-def sprawdzanie(digits):
-    global d1
-    for d1 in range(0, len(digits)):
-        for d2 in range(0, d1):
-            if digits[d1] == digits[d2] and d1 != d2:
-                return digits[d1] 
-print (sprawdzanie(digits))      
+digits = [7, 6, 4, 3, 1, 1, 7, 3, 6, 8, 8, 9,]
+def repeating_digit(digits):
+    for first_digit in range(0, len(digits)):
+        for second_digit in range(0, first_digit):
+            if digits[first_digit] == digits[second_digit] and first_digit != second_digit:
+                return digits[first_digit]                                                 
+print (repeating_digit(digits)) 
