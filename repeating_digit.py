f64571077d9bf300d77dff74654048c0b61d9918
Krzysztof <krzysztof.majchrzak12@gmail.com>
2018-09-08 23:53:05 +0200

    indentation added; digits is now after function

--- repeating_digit.py
+++ repeating_digit.py
@@ -1,7 +1,8 @@
-digits = [7, 6, 4, 3, 1, 1, 7, 3, 6, 8, 8, 9,]
 def repeating_digit(digits):
     for first_digit in range(0, len(digits)):
         for second_digit in range(0, first_digit):
             if digits[first_digit] == digits[second_digit] and first_digit != second_digit:
-                return digits[first_digit]                                                 
+                return digits[first_digit]
+
+digits = [7, 6, 4, 3, 1, 1, 7, 3, 6, 8, 8, 9,]                                                 
 print (repeating_digit(digits)) 
