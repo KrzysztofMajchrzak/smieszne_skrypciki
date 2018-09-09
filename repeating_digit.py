d4b88f7f70500d1c89d8d90fc285817d67db5987
Krzysztof <krzysztof.majchrzak12@gmail.com>
2018-09-09 19:30:21 +0200

    shows 2 values: first element of the list, second index of first repeated element

--- repeating_digit.py
+++ repeating_digit.py
@@ -1,8 +1,9 @@
 def repeating_digit(digits):
     for first_digit in range(0, len(digits)):
         for second_digit in range(0, first_digit):
+            index_1 = digits[second_digit]
             if digits[first_digit] == digits[second_digit] and first_digit != second_digit:
-                return digits[first_digit]
-
-digits = [7, 6, 4, 3, 1, 1, 7, 3, 6, 8, 8, 9,]                                                 
-print (repeating_digit(digits)) 
+                return digits[first_digit], digits.index(index_1)
+                
+digits = [4, 4, 6, 4, 3, 4, 6, 2, 1, 7, 3, 6, 8, 8, 9,]                                                 
+print (repeating_digit(digits))   
