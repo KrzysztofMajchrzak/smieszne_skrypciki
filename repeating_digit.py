747339c65ddeb8e50be2fbbee71c83f143f12874
Krzysztof <krzysztof.majchrzak12@gmail.com>
2018-09-09 21:25:23 +0200

    names for function, list and variables changed; now funtion shows also index of the second digit that repeats in list

--- repeating_digit.py
+++ repeating_digit.py
@@ -1,9 +1,9 @@
-def repeating_digit(digits):
-    for first_digit in range(0, len(digits)):
-        for second_digit in range(0, first_digit):
-            index_1 = digits[second_digit]
-            if digits[first_digit] == digits[second_digit] and first_digit != second_digit:
-                return digits[first_digit], digits.index(index_1)
+def RepeatingDigit(list_of_digits):
+    for first_index in range(0, len(list_of_digits)):
+        for second_index in range(0, first_index):
+            index_1 = list_of_digits[second_index]
+            if list_of_digits[first_index] == list_of_digits[second_index] and first_index != second_index:
+                return list_of_digits[first_index], second_index, first_index
                 
-digits = [4, 4, 6, 4, 3, 4, 6, 2, 1, 7, 3, 6, 8, 8, 9,]                                                 
-print (repeating_digit(digits))   
+list_of_digits = [4, 4, 6, 4, 3, 4, 6, 2, 1, 7, 3, 6, 8, 8, 9,]                                                 
+print (RepeatingDigit(list_of_digits))   
