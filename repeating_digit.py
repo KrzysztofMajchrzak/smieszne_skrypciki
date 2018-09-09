b6f93d29892720aa0122f8f5b6eb3959773c3f69
Krzysztof <krzysztof.majchrzak12@gmail.com>
2018-09-09 22:14:53 +0200

    first_index renamed as second_index and vice versa; docstring added

--- repeating_digit.py
+++ repeating_digit.py
@@ -1,8 +1,14 @@
 def RepeatingDigit(list_of_digits):
-    for first_index in range(0, len(list_of_digits)):
-        for second_index in range(0, first_index):
-            if list_of_digits[first_index] == list_of_digits[second_index] and first_index != second_index:
-                return list_of_digits[first_index], second_index, first_index
+    """
+    Returns first duplicate from the list and indexes of the first two same elements
+
+    Arguments:
+    list_of_digits - list with digits from 0 to 9 (included); must contain at least one pair of the same elements
+    """	
+    for second_index in range(0, len(list_of_digits)):
+        for first_index in range(0, second_index):
+            if list_of_digits[second_index] == list_of_digits[first_index] and first_index != second_index:
+                return list_of_digits[second_index], first_index, second_index
                 
 list_of_digits = [4, 4, 6, 4, 3, 4, 6, 2, 1, 7, 3, 6, 8, 8, 9,]                                                 
 print (RepeatingDigit(list_of_digits))   
