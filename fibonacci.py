309e4411d9379fb4a80607c680b9cccb798adc11
Krzysztof <krzysztof.majchrzak12@gmail.com>
2018-09-19 22:43:51 +0200

    first function looks better, third function added, second one somehow doesn't work properly

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,33 @@
+from math import sqrt 
 def Fibonacci(n):
 
-    if n == 0: return 0
+	if n == 0: 
+		return 0
     
-    elif n == 1: return 1
+	elif n == 1: 
+		return 1
     
-    else: return Fibonacci (n - 2) + Fibonacci (n - 1)
+	else: 
+		return Fibonacci (n - 2) + Fibonacci (n - 1)
 
 def Fib(n):
 
-    n = (1 / (5**(1/2)))*(((1 + (5**(1/2))) / 2))**n - (1 / (5**(1/2)))*(((1 - (5**(1/2))) / 2))**n
-    return round(n)
+    k = (1 / (sqrt(5)))*(((1 + (sqrt(5))) / 2))**n
+    return round(k)
 
+def Fibon(n):
+	a = 0
+	b = 1 
+	d = 1
+	while d != n:
+		c = a + b
+		a = b 
+		b = c
+		d = d + 1 
+	return c
+	
 
-print (Fibonacci(5))
-print (Fib(5))
+print (Fibonacci(15))
+print (Fib(100))
+print (Fibon(100))
 
